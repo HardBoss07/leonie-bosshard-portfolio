74b9bb0071fdf76df14bf95e7f0062c5581ab883
HardBoss07 <matteo@bosshard.net>
2026-02-12 16:26:50 +0100

    added lib for categories and util functions

---
+++ lib/constants/categories.ts
@@ -0,0 +1,8 @@
+import { ProjectCategory } from "@/types/project";
+
+export const PROJECT_CATEGORIES: ProjectCategory[] = [
+  "Graphic Design",
+  "Photography",
+  "UX / UI Design",
+  "Analog Design",
+];
---
+++ lib/utils/get-lucide-icon.ts
@@ -0,0 +1,25 @@
+import { Figma, Camera, Palette, LucideProps, LucideIcon } from 'lucide-react';
+import React from 'react';
+
+interface IconProps extends LucideProps {
+  name: string;
+}
+
+const icons: { [key: string]: LucideIcon } = {
+  Figma: Figma,
+  Camera: Camera,
+  Palette: Palette,
+};
+
+const Icon = ({ name, ...props }: IconProps) => {
+  const LucideIconComponent = icons[name];
+
+  if (!LucideIconComponent) {
+    console.warn(`Icon '${name}' not found.`);
+    return null;
+  }
+
+  return React.createElement(LucideIconComponent, props);
+};
+
+export default Icon;
