b38f3d0a5433f18a59ed603bc137714ea68e887c
HardBoss07 <matteo@bosshard.net>
2026-02-24 09:44:22 +0100

    added new logo and updated favicon to use logo-zoom.svg

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ export const metadata: Metadata = {
   title: "Portfolio | Leonie Bosshard",
   description:
     "Leonie Bosshard - Professionelle Fotografin und UI/UX-Designerin mit Leidenschaft für kreative Projekte und innovative Lösungen.",
+  icons: {
+    icon: "/icons/logo-zoom.svg",
+  }
 };
 
 export default function RootLayout({
