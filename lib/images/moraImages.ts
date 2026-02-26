76d387826611390078feeae550064a304d2b6f9b
HardBoss07 <matteo@bosshard.net>
2026-02-26 10:01:00 +0100

    added image collection files for each project

---
+++ lib/images/moraImages.ts
@@ -0,0 +1,11 @@
+import { IMAGE_PREFIX } from "@/lib/constants/imagePrefix";
+import { ImageData } from "@/types/image";
+
+export const moraImages: ImageData[] = [
+  { src: `${IMAGE_PREFIX[1]}mora_1_4000x3000.png`, alt: "Mora 1", w: 4000, h: 3000 },
+  { src: `${IMAGE_PREFIX[1]}mora_2_4000x3000.png`, alt: "Mora 2", w: 4000, h: 3000 },
+  { src: `${IMAGE_PREFIX[1]}mora_3_3400x3200.png`, alt: "Mora 3", w: 3400, h: 3200 },
+  { src: `${IMAGE_PREFIX[1]}mora_4_4000x2667.png`, alt: "Mora 4", w: 4000, h: 2667 },
+  { src: `${IMAGE_PREFIX[1]}mora_5_4000x3000.png`, alt: "Mora 5", w: 4000, h: 3000 },
+  { src: `${IMAGE_PREFIX[1]}mora_6_4000x3000.png`, alt: "Mora 6", w: 4000, h: 3000 },
+];
---
+++ lib/images/redentKitImages.ts
@@ -0,0 +1,11 @@
+import { IMAGE_PREFIX } from "@/lib/constants/imagePrefix";
+import { ImageData } from "@/types/image";
+
+export const redentKitImages: ImageData[] = [
+    { src: `${IMAGE_PREFIX[9]}redent_kit_3_4096x4064.png`, alt: "Kit 1", w: 4096, h: 4064 },
+    { src: `${IMAGE_PREFIX[9]}redent_kit_1_4096x2731.png`, alt: "Kit 2", w: 4096, h: 2731 },
+    { src: `${IMAGE_PREFIX[9]}redent_kit_2_4096x2725.png`, alt: "Kit 3", w: 4096, h: 2725 },
+    { src: `${IMAGE_PREFIX[9]}redent_kit_5_1900x1900.png`, alt: "Kit 4", w: 1900, h: 1900 },
+    { src: `${IMAGE_PREFIX[9]}redent_kit_6_2731x2731.png`, alt: "Kit 5", w: 2731, h: 2731 },
+    { src: `${IMAGE_PREFIX[9]}redent_kit_4_4096x2731.png`, alt: "Kit 6", w: 4096, h: 2731 },
+];
